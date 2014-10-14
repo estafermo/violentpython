090a55db3888a9058c94b3819a7803adc11ab3d8
impious <d.impious@gmail.com>
2014-10-14 22:45:25 +0100

    unzip.py

--- unzip.py
+++ unzip.py
@@ -1,20 +1,36 @@
 import zipfile
+import optparse
+from threading import Thread
 
 def extractFile(zfile,password):
 	try:
 		zfile.extractall(pwd=password)
-		return password
+		#return password
+		print '[+] Found password '+password
 	except:
-		print '[+] failed password='+password
-		return
+		pass
+		#print '[+] failed password='+password
+		#return
 def main():
-	zFile=zipfile.ZipFile('evil.zip')
-	passFile=open('dictionary.txt')
+	parser = optparse.OptionParser("usage%prog-f <zipfile> -d <dictionary>")
+	parser.add_option('-f', dest='zname', type='string',help='specify zip file')
+	parser.add_option('-d', dest='dname', type='string',help='specify dictionary file')
+	(options, args) = parser.parse_args()
+	if (options.zname == None) | (options.dname == None):
+		print parser.usage
+		exit(0)
+	else:
+		zname = options.zname
+		dname = options.dname
+	zFile=zipfile.ZipFile(zname)
+	passFile=open(dname)
 	for line in passFile.readlines():
 		password=line.strip('\n')
-		guess=extractFile(zFile,password)
-		if guess:
-			print '[+] Password='+password
-			exit(0)
+		t = Thread(target=extractFile, args=(zFile, password))
+		t.start()
+		#guess=extractFile(zFile,password)
+		#if guess:
+		#	print '[+] Password='+password
+		#	exit(0)
 if __name__=='__main__':
 	main()
